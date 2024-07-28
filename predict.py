16e1914392afc30d7196111b0bde0a50750c07f9
7Alexandra8 <alexp20030708@gmail.com>
2024-07-28 21:11:10 +0300

    починка багов

--- predict.py
+++ predict.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import joblib
 
-# Загрузка модели и векторизатора
+# 1. Загрузка модели и векторизатора
 model = joblib.load('model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
-# Загрузка тестовых данных
+# 2. Загрузка тестовых данных
 test_df = pd.read_csv('test.csv')
 test_domains = test_df['domain']
 
-# Преобразование и предсказание
+# 3. Преобразование и предсказание
 X_test = vectorizer.transform(test_domains)
 predictions = model.predict(X_test)
 
-# Сохранение результатов
+# 4. Сохранение результатов
 output_df = pd.DataFrame({'domain': test_domains, 'is_dga': predictions})
 output_df.to_csv('prediction.csv', index=False)
 
